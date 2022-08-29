6f579c002f147943ef5a2eb0a5f9792b8f26e315
DeadInside1985 <76182083+DeadInside1985@users.noreply.github.com>
2022-08-29 04:20:15 +0300

    Update writer.go

--- writer.go
+++ writer.go
@@ -204,8 +204,9 @@ func (p *MasterPlaylist) Encode() *bytes.Buffer {
 			}
 			p.buf.WriteRune('\n')
 		} else {
-			p.buf.WriteString("#EXT-X-STREAM-INF:")
-			p.buf.WriteString("BANDWIDTH=")
+			p.buf.WriteString("#EXT-X-STREAM-INF:PROGRAM-ID=")
+			p.buf.WriteString(strconv.FormatUint(uint64(pl.ProgramId), 10))
+			p.buf.WriteString(",BANDWIDTH=")
 			p.buf.WriteString(strconv.FormatUint(uint64(pl.Bandwidth), 10))
 			if pl.AverageBandwidth != 0 {
 				p.buf.WriteString(",AVERAGE-BANDWIDTH=")
